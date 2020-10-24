dbeced7178a93903eb85f58d7e5e4ec0e16d1116
Radley <radleyroy@gmail.com>
2020-10-24 18:57:59 +0530

    booking done

--- management/models.py
+++ management/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class Vehicle(models.Model):
@@ -14,3 +15,11 @@ class Vehicle(models.Model):
 
     def __str__(self):
         return (self.make + ' ' +self.model)
+
+class Booking(models.Model):
+    bookingID = models.AutoField(primary_key=True)
+    VehicleID = models.ForeignKey(Vehicle, on_delete=models.CASCADE)
+    userID = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    hours = models.IntegerField()
